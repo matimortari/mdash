67f3c6926fa356bcf7b9fc4787bee185c16ed564
matheus <matheus.felipe.19rt@gmail.com>
2024-08-31 15:19:15 -0300

    chore: Install & use Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import EditorContainer from "./components/EditorContainer"
 import Footer from "./components/Footer"
 import TopNav from "./components/TopNav"
@@ -5,6 +6,7 @@ import TopNav from "./components/TopNav"
 export default function Home() {
 	return (
 		<>
+			<Analytics />
 			<TopNav />
 			<main className="flex flex-col items-center justify-center">
 				<EditorContainer />
